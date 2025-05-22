0f797460bead6709b0d7fcb9c4519a1be9090c68
Daniela-1569 <danielavalladolid04@gmail.com>
2025-05-22 18:41:25 -0500

    Prueba 22/05/2025

---
+++ src/main/java/com/mycompany/daniela/Daniela.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.daniela;
+
+import static java.awt.SystemColor.menu;
+import static java.time.InstantSource.system;
+
+/**
+ *
+ * @author PC01
+ */
+public class Daniela {
+
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(system.int);
+       
+        System.out.println("Ingrese el tipo de mascota");
+        System.out.println("1. Perro");
+        System.out.println("2, Gato");
+        System.out.println("3. Ave");
+        System.out.println("Opciones");
+        
+ 
+        
+        
+        
+    }
+}
